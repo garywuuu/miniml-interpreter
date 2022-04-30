a30c4f378f8e9843607203cfccc3b76ee7d944b8
garywu <garywu@college.harvard.edu>
2022-04-30 16:07:45 -0400

    lol

--- expr.ml
+++ expr.ml
@@ -61,7 +61,13 @@ let vars_of_list : string list -> varidset =
 (* free_vars exp -- Returns the set of `varid`s corresponding to free
    variables in `exp` *)
 let free_vars (exp : expr) : varidset =
-  failwith "free_vars not implemented" ;;
+  match exp with 
+  | Num _
+  | Bool _ 
+  | Unop (Negate, _) 
+  | Raise 
+  | Unassigned -> SS.empty 
+  Var ;;
   
 (* new_varname () -- Returns a freshly minted `varid` constructed with
    a running counter a la `gensym`. Assumes no variable names use the
@@ -90,10 +96,49 @@ let subst (var_name : varid) (repl : expr) (exp : expr) : expr =
    
 (* exp_to_concrete_string exp -- Returns a string representation of
    the concrete syntax of the expression `exp` *)
-let exp_to_concrete_string (exp : expr) : string =
-  failwith "exp_to_concrete_string not implemented" ;;
+let rec exp_to_concrete_string (exp : expr) : string =
+  match exp with 
+  | Var varid -> varid       
+  | Num n -> string_of_int n         
+  | Bool b -> string_of_bool b               
+  | Unop (Negate, Num n ) -> string_of_int ~-n  
+  | Unop (Negate, _) -> "Error: number must be positive"             
+  | Binop (bin, e1, e2) -> let b = match bin with 
+                            | Plus -> " + " 
+                            | Times ->  " * " 
+                            | Minus -> " - " 
+                            | Equals -> " = " 
+                            | LessThan -> " < " in "(" ^ exp_to_concrete_string e1 ^ b ^ exp_to_concrete_string e2 ^ ")"
+  | Conditional (e1, e2, e3) -> "if" ^ exp_to_concrete_string e1 ^ "then" ^ exp_to_concrete_string e2 ^
+                                "else" ^ (exp_to_concrete_string e3)     
+  | Fun (v, e) -> "let " ^ v ^ " = " ^ exp_to_concrete_string e                  
+  | Let (va, e1, e2) -> "let " ^ va ^ " = " ^ exp_to_concrete_string e1 ^ " in " ^ exp_to_concrete_string e2          
+  | Letrec (va, e1, e2) -> "let rec " ^ va ^ " = " ^ exp_to_concrete_string e1 ^ "in" ^ exp_to_concrete_string e2     
+  | Raise -> "parse error"                              
+  | Unassigned -> "unassigned"                            
+  | App (e1, e2) -> exp_to_concrete_string e1 ^ " " ^ exp_to_concrete_string e2
+  ;;
      
 (* exp_to_abstract_string exp -- Return a string representation of the
    abstract syntax of the expression `exp` *)
-let exp_to_abstract_string (exp : expr) : string =
-  failwith "exp_to_abstract_string not implemented" ;;
+let rec exp_to_abstract_string (exp : expr) : string =
+  match exp with 
+  | Var varid -> "Var(" ^ varid ^ ")"     
+  | Num n ->  "Num(" ^ string_of_int n^ ")"                              
+  | Bool b-> if b then "Bool(true)" else "Bool(false)"                          
+  | Unop (Negate, Num i) -> "Unop(Negate(" ^ string_of_int i ^ ")"  
+  | Unop (Negate, _) -> "Error: number must be positive"
+  | Binop (bin, e1, e2) -> let b = match bin with 
+                            | Plus -> " + "
+                            | Times -> " * " 
+                            | Minus -> " - " 
+                            | Equals -> " = " 
+                            | LessThan -> " < " in exp_to_abstract_string e1 ^ b ^ exp_to_abstract_string e2  
+  | Conditional (e1, e2, e3) -> "Conditional(" ^ exp_to_abstract_string e1 ^ ", " ^ exp_to_abstract_string e2 ^ ", " ^ exp_to_abstract_string e3 ^ ")"
+  | Fun (va, e) -> "Fun(" ^ va ^ ", " ^ exp_to_abstract_string e ^ ")"       
+  | Let (va, e1, e2) -> "Let(" ^ va ^ ", " ^ exp_to_abstract_string e1 ^ ", " ^ exp_to_abstract_string e2 ^ ")"  
+  | Letrec (va, e1, e2) -> "Letrec(" ^ va ^ ", " ^ exp_to_abstract_string e1 ^ ", " ^ exp_to_abstract_string e2 ^ ")" 
+  | Raise -> "parse error"                           
+  | Unassigned -> "unassigned"                          
+  | App (e1, e2) ->  "App(" ^ exp_to_abstract_string e1 ^ ", " ^ exp_to_abstract_string e2 ^ ")"
+  ;;
